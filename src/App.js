14cd3c74db03c928d7e87722f846f6ecdbbcc40a
Fady ElHalfawy <fadyhalfawy@gmail.com>
2022-01-19 09:03:21 +0200

    Add App.js component.

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import Review from "./components/Review";
+
+const App = () => {
+    return (
+    <main>
+        <section className={"container"}>
+
+            <div className={"title"}>
+                <h2>Reviews</h2>
+                <div className={"underline"}></div>
+            </div>
+
+            <Review />
+        </section>
+    </main>
+    );
+}
+
+export default App;
